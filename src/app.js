c8dfd48cf8a51be0ba96e2aa056cf4fd66d056aa
Gilles GRIFFIT <griffitstudio@hotmail.com>
2025-06-20 23:56:13 -0300

    Modif .eslintrc

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use((req, res) => {
 });
 
 // Gestion globale des erreurs serveur
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Erreur serveur interne' });
 });
